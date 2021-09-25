09f5794497de327571ba5666d5761bab089adad2
파링 <parangee9706@gmail.com>
2021-09-25 20:18:37 +0900

    add dokdo

--- src/structures/client.ts
+++ src/structures/client.ts
@@ -1,12 +1,13 @@
 import { CommandClient } from '@pikokr/command.ts'
-import Discord, { Intents, IntentsString } from 'discord.js'
+import Discord, { Intents, IntentsString, Message } from 'discord.js'
 import { config } from '../config'
+import Dokdo from 'dokdo'
 
 export class Client extends CommandClient {
     constructor() {
         super({
             client: new Discord.Client({
-                intents: Object.keys(Intents.FLAGS).filter(x=>!(['GUILD_MEMBERS', 'GUILD_PRESENCES'] as IntentsString[]).includes(x as IntentsString)) as IntentsString[],
+                intents: Object.keys(Intents.FLAGS).filter((x) => !(['GUILD_MEMBERS', 'GUILD_PRESENCES'] as IntentsString[]).includes(x as IntentsString)) as IntentsString[],
             }),
             owners: 'auto',
             command: {
@@ -19,5 +20,14 @@ export class Client extends CommandClient {
         })
 
         this.registry.loadModulesIn('modules')
+    }
+
+    async ready(): Promise<void> {
+        await super.ready()
+        new Dokdo(this.client, {
+            prefix: '.',
+            noPerm(message: Message): any {},
+            owners: this.owners,
+        })
     }
 }
